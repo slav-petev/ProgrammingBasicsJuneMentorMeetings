553eed71d011640d4d751a3df64aea8446e32878
Slav Petkov <Slav Petkov@PC-1138>
2016-07-11 20:01:57 +0300

    Solved Repair The Tiles problem

---
+++ MentorMeting11July2016OnlineEvening/RepairingTheTiles/RepairingTheTilesExcercise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairingTheTiles
+{
+    class RepairingTheTilesExcercise
+    {
+        static void Main(string[] args)
+        {
+            int platformSideLength = int.Parse(Console.ReadLine());
+            double tileWidth = double.Parse(Console.ReadLine());
+            double tileLength = double.Parse(Console.ReadLine());
+            int benchWidth = int.Parse(Console.ReadLine());
+            int benchLength = int.Parse(Console.ReadLine());
+
+            int platformArea = platformSideLength * platformSideLength;
+            int benchArea = benchWidth * benchLength;
+            int platformAreaToBeCoveredWithTiles = platformArea - benchArea;
+            double tileArea = tileWidth * tileLength;
+
+            double numberOfTilesToCoverThePlatform = platformAreaToBeCoveredWithTiles / tileArea;
+
+            double timeNeededToCoverThePlatform = numberOfTilesToCoverThePlatform * 0.2;
+
+            Console.WriteLine(numberOfTilesToCoverThePlatform);
+            Console.WriteLine(timeNeededToCoverThePlatform);
+        }
+    }
+}
