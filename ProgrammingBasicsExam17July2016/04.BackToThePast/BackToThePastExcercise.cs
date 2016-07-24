86aa57a12666aa8502cbae4a777b4780009979d1
Slav Petkov <Slav Petkov@PC-1138>
2016-07-24 15:49:35 +0300

    Solved problem Back To The Past

---
+++ ProgrammingBasicsExam17July2016/04.BackToThePast/BackToThePastExcercise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BackToThePast
+{
+    public class BackToThePastExcercise
+    {
+        static void Main()
+        {
+            decimal inheritedMoneyAmount = decimal.Parse(Console.ReadLine());
+            int finalYearToLiveInThePast = int.Parse(Console.ReadLine());
+
+            const int startYearToLiveInThePast = 1800;
+            const decimal baseExpensesPerYear = 12000M;
+            const int ageAtStartOfPeriod = 18;
+            decimal totalExpensesForTimeInThePast = decimal.Zero;
+            for (int currentYearInThePast = startYearToLiveInThePast;
+                    currentYearInThePast <= finalYearToLiveInThePast; currentYearInThePast++)
+            {
+                if (currentYearInThePast % 2 == 0)
+                {
+                    totalExpensesForTimeInThePast += baseExpensesPerYear;
+                }
+                else
+                {
+                    int currentAge = ageAtStartOfPeriod +
+                        (currentYearInThePast - startYearToLiveInThePast);
+                    decimal currentYearExpenses = baseExpensesPerYear +
+                        50 * currentAge;
+                    totalExpensesForTimeInThePast += currentYearExpenses;
+                }
+            }
+
+            decimal moneyDifference = Math.Abs(
+                inheritedMoneyAmount - totalExpensesForTimeInThePast);
+            if (inheritedMoneyAmount >= totalExpensesForTimeInThePast)
+            {
+                Console.Write("Yes! He will live a carefree life and will have ");
+                Console.WriteLine(
+                    $"{moneyDifference:F2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {moneyDifference:F2} dollars to survive.");
+            }
+        }
+    }
+}
