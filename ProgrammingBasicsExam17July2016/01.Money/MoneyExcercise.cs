e2ab50a32a24e27d4368802e843f02bbd6ed56bb
Slav Petkov <Slav Petkov@PC-1138>
2016-07-24 14:18:21 +0300

    Solved problem Money with errors

---
+++ ProgrammingBasicsExam17July2016/01.Money/MoneyExcercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.Money
+{
+    public class MoneyExcercise
+    {
+        static void Main()
+        {
+            int bitCoinsCount = int.Parse(Console.ReadLine());
+            decimal chineseYansCount = decimal.Parse(Console.ReadLine());
+            decimal changeBuroCommissionRate = decimal.Parse(Console.ReadLine());
+
+            const decimal bgnInBitCoin = 1168;
+            decimal bgnFromBitCoinsAmount = bitCoinsCount * bgnInBitCoin;
+            const decimal dollarsInChineseYuan = 0.15M;
+            decimal dollarsFromChineseYuansAmount = chineseYansCount * dollarsInChineseYuan;
+            const decimal bgnInDollar = 1.76M;
+            decimal bgnFromChineseYuansAmount = dollarsFromChineseYuansAmount * bgnInDollar;
+
+            decimal totalBgnAmount = bgnFromBitCoinsAmount + bgnFromChineseYuansAmount;
+            const decimal bgnInEuro = 1.95M;
+            decimal totalEuroAmount = totalBgnAmount / bgnInEuro;
+
+            decimal commissionAmount = (changeBuroCommissionRate / 100) * totalEuroAmount;
+            decimal netEuroAmount = totalEuroAmount - commissionAmount;
+
+            Console.WriteLine($"{netEuroAmount:F12}");
+        }
+    }
+}
