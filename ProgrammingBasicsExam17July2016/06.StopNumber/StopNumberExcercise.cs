39a8ed4f23ed6aa82053dc2ee02a40a6180606b8
Slav Petkov <Slav Petkov@PC-1138>
2016-07-24 16:40:10 +0300

    Solved problem StopNumber

---
+++ ProgrammingBasicsExam17July2016/06.StopNumber/StopNumberExcercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.StopNumber
+{
+    public class StopNumberExcercise
+    {
+        static void Main()
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+            int stopNumber = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = lastNumber;
+                currentNumber >= firstNumber; currentNumber--)
+            {
+                if (currentNumber % 6 == 0)
+                {
+                    if (currentNumber == stopNumber)
+                    {
+                        break;
+                    }
+
+                    Console.Write("{0} ", currentNumber);
+                }
+            }
+        }
+    }
+}
